50cfd7705ba3d777d5bb608df1acf046316cae86
Abishek N <151867893+Abisheknesa@users.noreply.github.com>
2025-07-28 20:40:49 +0530

    Create learning2.sql

---
+++ learning2.sql
@@ -0,0 +1,41 @@
+<--//**selection**//-->
+
+<--11.WAQTD NAME AND SALARY GIVEN TO AN EMPLOYEE IF       EMPLOYEE EARNS  A COMMISSION OF RUPEES 1400 
+12.WAQTD DETAILS OF EMPLOYEES HAVING COMMISSION MORE     THAN SALARY 
+13.WAQTD EMPNO OF EMPLOYEES HIRED BEFORE THE YEAR 87
+14.WAQTD DETAILS OF EMPLOYEES WORKING AS AN N ANALYST
+15.WAQTD DETAILS OF EMPS EARNING MORE THAN 2000 RUPEES PER MONTH  -->
+
+--sol11--
+ENAME             SAL
+---------- ----------
+MARTIN           1250
+
+--sol12--
+ENAME
+----------
+MARTIN
+
+--sol13--
+     EMPNO
+----------
+      7788
+      7876
+  
+--sol14--
+
+     EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO
+---------- ---------- --------- ---------- --------- ---------- ---------- ----------
+      7788 SCOTT      ANALYST         7566 19-APR-87       3000                    20
+      7902 FORD       ANALYST         7566 03-DEC-81       3000                    20
+
+--sol15--
+    EMPNO ENAME      JOB              MGR HIREDATE         SAL       COMM     DEPTNO
+--------- ---------- --------- ---------- --------- ---------- ---------- ----------
+     7566 JONES      MANAGER         7839 02-APR-81       2975                    20
+     7698 BLAKE      MANAGER         7839 01-MAY-81       2850                    30
+     7782 CLARK      MANAGER         7839 09-JUN-81       2450                    10
+     7788 SCOTT      ANALYST         7566 19-APR-87       3000                    20
+     7839 KING       PRESIDENT            17-NOV-81       5000                    10
+     7902 FORD       ANALYST         7566 03-DEC-81       3000                    20
+
